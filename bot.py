1303da7e9578ff9fd2b2619196e5d2d6238cf68d
Theblvcksheep <34955065+Theblvcksheep@users.noreply.github.com>
2023-04-04 20:30:51 +0200

    GA v1.2

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+random.seed(20516)
 class Individual:
     dictionary = [['R', 'R', 'R', 'R'], ['R', 'R', 'R', 'P'],
               ['R', 'R', 'R', 'S'], ['R', 'R', 'P', 'R'],
@@ -53,22 +54,43 @@ class Individual:
         self.win = 0
         self.lose = 0
         self.draw = 0
+        self.untested = 0 
         self.dna = {}
         for item in self.dictionary:
-            self.dna[str(item)] = random.choice(['R', 'P', 'S'])
+            self.dna["".join(item)] = [random.choice(['R', 'P', 'S']), 3]
 
-    def get_fitness(self)->int:
-        self.fitness = self.win * 5 - self.lose * 2 - self.draw * 1
+    def get_fitness(self, Ideal = False)->int:
+        if Ideal:
+            self.win = 81
+            self.fitness = int(((self.win/81)*100))* 5
+            return self.fitness
+        
+        self.win = 0 
+        self.draw = 0
+        self.lose = 0
+        self.untested = 0
+        for keys, values in self.dna.items():
+            if values[1] == -1:
+                self.lose += 1
+            elif values[1] == 1:
+                self.win += 1
+            elif values[1] == 0:
+                self.draw += 1
+            else:
+                self.untested += 1
+
+        self.fitness = int(((self.win/81)*100)) * 5 - int(((self.lose/81)*100)) * 2 - int(((self.draw/81)*100)) 
         return self.fitness
 
     def crossover(self, other):
         baby = Individual()
+        mid = random.randint(27, 54)
         i = 0
-        for key in self.dna.keys():
-            if(i < 40):
-                baby.dna.update({key : self.dna.get(key)}) 
+        for key in baby.dna.keys():
+            if i < mid:
+                baby.dna[key] = self.dna.get(key)
             else:
-                baby.dna.update({key : other.dna.get(key)}) 
+                baby.dna[key] = other.dna.get(key)
             i += 1
 
         return baby
@@ -84,18 +106,18 @@ class Individual:
                 mutateGene += random.choice(['R', 'P', 'S'])
         
             if self.dna.get(mutateGene) == "P":
-                self.dna[mutateGene] = random.choice(['R', 'S'])
+                self.dna[mutateGene] = [random.choice(['R', 'S']), 3]
             elif self.dna.get(mutateGene) == "S":
-                self.dna[mutateGene] = random.choice(['R', 'P'])
+                self.dna[mutateGene] = [random.choice(['R', 'P']), 3]
             else:
-                self.dna[mutateGene] = random.choice(['P', 'S'])
-
+                self.dna[mutateGene] = [random.choice(['P', 'S']), 3]
             mutateGene = ""
         return 0
+    
     def __str__(self) -> str:
         str = ""
-        for key, value in self.dna:
-            if key == "SSSS":
+        for key, value in self.dna.items():
+            if key == 'SSSS':
                 str += value
             else:
                 str += value + ","
@@ -104,18 +126,23 @@ class Individual:
 
 
 class Envirnoment:
-    def __init__(self, populationSize, envirnoment, mutateRate) -> None:
+    def __init__(self, populationSize, envirnoment, mutateRate, el, el_rate) -> None:
         self.size = populationSize
         self.rate = mutateRate
+        self.elite_flag = el
+        self.elite_rate = el_rate
+        self.elites = []
+        self.ideal = 0
         self.pop = []
         self.matingPool = []
-        self.avgFitPerGene = []
+        self.idealFitPerGene = []
         self.sumFitPerGen = []
-        self.avgPosFitPerGene = []
         self.env = envirnoment
         self.envSize = len(envirnoment)
         self.gen_itr = self.envSize//(81 * self.size)
-         
+        self.max_individaul = Individual()
+        self.repopulate()
+        self.sim_ideal()
         self.repopulate()
 
     def repopulate(self):
@@ -124,38 +151,66 @@ class Envirnoment:
 
     def fitness_test(self, ind, gene):
         gene_test = str(gene[0])
+        
+        if ind.dna.get(gene_test)[0] == "P":
+            if gene[1] == "P":
+                ind.dna[gene_test][1] = 0
+            elif gene[1] == "S":
+                ind.dna[gene_test][1] = -1
+            else:
+                ind.dna[gene_test][1] = 1
 
-        if ind.dna.get(gene_test) == "P":
+        elif ind.dna.get(gene_test)[0] == "S":
+           
             if gene[1] == "P":
-                ind.draw += 1
+                ind.dna[gene_test][1] = 1
             elif gene[1] == "S":
-                ind.lose += 1
+                ind.dna[gene_test][1] = 0
             else:
-                ind.win += 1
-        elif ind.dna.get(gene_test) == "S":
-            if gene[1] == "P":
-                ind.win += 1
-            elif gene[1] == "S":
-                ind.draw += 1
-            else:
-                ind.lose += 1
+                ind.dna[gene_test][1] = -1
         else:
             if gene[1] == "P":
-                ind.lose += 1
+                ind.dna[gene_test][1] = -1
             elif gene[1] == "S":
-                ind.win += 1
+                ind.dna[gene_test][1] = 1
             else:
-                ind.draw += 1
+                ind.dna[gene_test][1] = 0
 
             
     def test_individaul(self, ind, test):
         for gene in test:
+            #print(f"Test Gene : {gene}. Before Ind Wins: {ind.win}, Loses : {ind.lose}, Draw : {ind.draw}")
             self.fitness_test(ind, gene)
+            ind.get_fitness()
+            #print(f"Test Gene : {gene}. After Ind Wins: {ind.win}, Loses : {ind.lose}, Draw : {ind.draw}")
+            #print("")
+
+    def sim_ideal(self):
+        for ind in self.pop:
+            self.ideal += ind.get_fitness(True)
+        self.pop.clear()
+
+    def get_max_individaul(self):
+        max = Individual()
+        for ind in self.pop:
+            if max.get_fitness() < ind.get_fitness():
+                max = ind
+        
+        if self.max_individaul.get_fitness() < max.get_fitness():
+            self.max_individaul = max
+
+        return max
     
+    def get_elites(self):
+        self.elites = [Individual()] * int(self.size * self.elite_rate)
+        for ind in self.pop:
+            if ind.get_fitness() > min([elite.get_fitness() for elite in self.elites]):
+                self.elites.remove(min(self.elites, key=lambda elite: elite.get_fitness()))
+                self.elites.append(ind)
+
     def test_env(self):
        
         for i in range(0, self.gen_itr):
-            pos_sum_fitness = 0
             total_fitness = 0
             self.matingPool = []
 
@@ -164,75 +219,96 @@ class Envirnoment:
                 test = []
                 for j in range(0, 81):
                     test.append(self.env.pop(0))
+
                 self.test_individaul(ind, test)
-                if ind.get_fitness() > 0:
-                    pos_sum_fitness += ind.get_fitness()
                 total_fitness += ind.get_fitness()
 
             ##################STATISTICS##################
-            totalAvg = total_fitness//len(self.pop)
-            self.avgFitPerGene.append(totalAvg)
+            idealFitPer = round((total_fitness/self.ideal) * 100, 2)
+            self.idealFitPerGene.append(idealFitPer)
             self.sumFitPerGen.append(total_fitness)    
-            self.avgPosFitPerGene.append(pos_sum_fitness)
-            print(f"Generation {i}: Total Fitness - {total_fitness}, Avg Fitness - {totalAvg}, Total Postive - {pos_sum_fitness} ")
+            print(f"Generation {i}: Total Fitness - {total_fitness}, Ideal Fitness - {self.ideal}, Ideal Precentage - {idealFitPer}%")
+            max = self.get_max_individaul()
+
+            print(f"Individaul with the highest fitness: {max.get_fitness()}, Selection Threshold: {max.get_fitness()//2}")
+            print(f"Max individual Win: {max.win}, Losses: {max.lose}, Draws: {max.draw}, Untested: {max.untested}")
             ##################STATISTICS##################
 
             # selection of individaul to enter mating pool
             for ind in self.pop:
-                if ind.get_fitness() > 0:
+                if ind.get_fitness() > max.get_fitness()//2:
                     n = ind.get_fitness()
                     for i in range(0, n):
                         self.matingPool.append(ind)
             
             self.pop = []
+            self.get_elites()
             for i in range(0, self.size):
-                a = random.randint(0, len(self.matingPool) - 1)
-                b = random.randint(0, len(self.matingPool) - 1)
-                #print(f"Mating Pool Length: {len(self.matingPool)}, parentA index: {a}, parentB index: {b}")
-                parentA = self.matingPool[a]
-                parentB = self.matingPool[b]
+                if self.elites and len(self.elites) != 0:
+                    baby = self.elites.pop(0)
+                    baby.win = 0
+                    baby.lose = 0
+                    baby.fitness = 0
+                    baby.draw = 0
+                    self.pop.append(baby)
+                else:    
+                    a = random.randint(0, len(self.matingPool) - 1)
+                    b = random.randint(0, len(self.matingPool) - 1)
+                    parentA = self.matingPool[a]
+                    parentB = self.matingPool[b]
 
-                baby = parentA.crossover(parentB)
-                baby.mutate(self.rate)
+                    baby = parentA.crossover(parentB)
+                    baby.mutate(self.rate)
 
-                self.pop.append(baby)
+                    self.pop.append(baby)
 
 data = []
-game = 'data1.csv'
-size = 100
-rate = 0.01
+game = 'data2.csv'
+size = 1000
+rate = 0.15
+elitism = True 
+eliteRate = 0.2
 with open(game, 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         data.append(row)
 
-game = Envirnoment(size, data, rate)
+game = Envirnoment(size, data, rate, elitism, eliteRate)
 game.test_env()
+max = game.max_individaul
+
+for key, value in max.dna.items():
+    if value[1] == 1:
+        print(f"Max individual DNA: History Gene {key}, Move {value[0]} -- WIN!! ")
+    elif value[1] == -1:
+        print(f"Max individual DNA: History Gene {key}, Move {value[0]} -- LOSS!! ")
+    elif value[1] == 0:
+        print(f"Max individual DNA: History Gene {key}, Move {value[0]} -- DRAW!! ")
+    else:
+        print(f"Max individual DNA: History Gene {key}, Move {value[0]} -- UNTESTED!! ")
 
 # create some dummy data for three datasets
 generation = np.arange(0, game.gen_itr)
-avgFit = np.array(game.avgFitPerGene)
+idealFit = np.array(game.idealFitPerGene)
 sumFit = np.array(game.sumFitPerGen)
 
-
-plt.subplot(2, 1, 1)
-# create a line plot with three different lines
-sns.lineplot(x=generation, y=sumFit, color='red', label='Dataset 2')
-
+plt.subplot( 2, 1, 1)
+#create a line plot with three different lines
+sns.lineplot(x=generation, y=sumFit, color='red', label='Dataset 2', marker='x')
 
 # set the title and axis labels
 plt.title('Total Fitness vs Generation')
 plt.xlabel('Generation')
 plt.ylabel('Fitness')
 
-# show the legend
-plt.legend()
+plt.subplot( 2, 1, 2)
+#create a line plot with three different lines
+sns.lineplot(x=generation, y=idealFit, color='red', label='Dataset 2', marker='o')
 
-plt.subplot(2, 1, 2)
-sns.lineplot(x=generation, y=avgFit, color='blue', label='Dataset 1')
-plt.title('Average Fitness vs Generation')
-plt.xlabel('Generation')
-plt.ylabel('Fitness')
+# set the title and axis labels
+#plt.title('Total Fitness vs Generation')
+plt.xlabel('Generation%')
+plt.ylabel('Ideal%')
 
 # show the plot
 plt.show()
